e83028edc8ecca75bd7dd6085cfa2e37392b73c5
yangzai360 <jieyangdh@gmail.com>
2017-12-08 11:15:27 +0800

    Pod install EmptyDataSet 完成

--- Podfile
+++ Podfile
@@ -9,4 +9,6 @@ target 'MyPlan' do
     pod 'SnapKit',    '~> 3.2.0'
     pod 'Eureka', :git => 'git://github.com/xmartlabs/Eureka', :commit => '9678ff95edd7b217ecb73c02938ce47d206cb03d'
     
+	pod 'DZNEmptyDataSet', '~> 1.8.1'
+    
 end
--- Pods/Target Support Files/Pods-MyPlan/Pods-MyPlan-frameworks.sh
+++ Pods/Target Support Files/Pods-MyPlan/Pods-MyPlan-frameworks.sh
@@ -102,10 +102,12 @@ strip_invalid_archs() {
 
 
 if [[ "$CONFIGURATION" == "Debug" ]]; then
+  install_framework "${BUILT_PRODUCTS_DIR}/DZNEmptyDataSet/DZNEmptyDataSet.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/Eureka/Eureka.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/SnapKit/SnapKit.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
+  install_framework "${BUILT_PRODUCTS_DIR}/DZNEmptyDataSet/DZNEmptyDataSet.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/Eureka/Eureka.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/SnapKit/SnapKit.framework"
 fi
